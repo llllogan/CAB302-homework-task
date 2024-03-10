f563c3ad3c904a51a0f51443e8697b4fa1407188
logan <logan@codify.com>
2024-03-10 18:06:38 +1000

    Added guard class, parsing input arguments to guard objects

--- main.java
+++ main.java
@@ -1,6 +1,11 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import obstacles.Guard;
+import obstacles.Obstacle;
+import obstacles.ObstacleType;
+
+
 class Main {
     public static void main(String[] args) {
         HashMap<String, ArrayList<String>> parsedArgs = parseArgs(args);
@@ -26,5 +31,35 @@ class Main {
             }
         }
         return parsedArgs;
+    }
+
+        /**
+     * Strips the parentheses from the argument
+     * @param arg The argument to strip
+     * @return The argument without parentheses
+     */
+    private static String stripParentheses(String arg) {
+        return arg.substring(1, arg.length() - 1);
+    }
+
+    /**
+     * Parses the obstacles from the command line arguments
+     * @param parsedArgs The parsed arguments
+     */
+    public static ArrayList<Obstacle> parseObstacles(HashMap<String, ArrayList<String>> parsedArgs) {
+        ArrayList<Obstacle> obstacles = new ArrayList<>();
+        ObstacleType type = ObstacleType.GUARD;
+        String key = "-" + type.getArgumentName();
+        ArrayList<String> args = parsedArgs.get(key);
+        if (args == null) {
+            return obstacles;
+        }
+        for (String arg : args) {
+            // Remove the parentheses from the argument
+            String cleanedArg = stripParentheses(arg);
+            Obstacle obstacle = Guard.parse(cleanedArg);
+            obstacles.add(obstacle);
+        }
+        return obstacles;
     }
 }
